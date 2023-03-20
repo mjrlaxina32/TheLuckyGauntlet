4f48aac57c6c59dc7cec99ee4a5e15570e4a7d49
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2023-03-20 08:57:15 +0800

    Update PauseController.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/PauseController.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/PauseController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.text.Text;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
@@ -15,10 +16,14 @@ import the_lucky_gauntlet.*;
 public class PauseController implements Initializable {
 	
 	@FXML ImageView mcPhoto, partnerPhoto, mcWeaponPhoto, partnerWeaponPhoto;
-	@FXML Text mcName, mcClass, mcAttack, mcHealth, mcEnergy;
-	@FXML Text partnerName, partnerClass, partnerAttack, partnerHealth, partnerEnergy;
-	@FXML Text mcWeaponName, mcWeaponClass, mcWeaponAttack, mcWeaponDurability;
-	@FXML Text partnerWeaponName, partnerWeaponClass, partnerWeaponAttack, partnerWeaponDurability;
+	@FXML Text mcName, mcClass, mcAttack;
+        @FXML ProgressBar mcHealth, mcEnergy;
+	@FXML Text partnerName, partnerClass, partnerAttack;
+        @FXML ProgressBar partnerHealth, partnerEnergy;
+	@FXML Text mcWeaponName, mcWeaponClass, mcWeaponAttack;
+        @FXML ProgressBar mcWeaponDurability;
+	@FXML Text partnerWeaponName, partnerWeaponClass, partnerWeaponAttack;
+        @FXML ProgressBar partnerWeaponDurability;
 	
 	Player mc, partner;
 	
@@ -30,23 +35,23 @@ public class PauseController implements Initializable {
 		mcName.setText(mc.getName());
 		mcClass.setText(mc.getClass().getSimpleName().substring(2));
 		mcAttack.setText("ATK: " + mc.getAttack());
-		mcHealth.setText(mc.getHealth() + "/" + mc.getMaxHealth());
-		mcEnergy.setText(mc.getEnergy() + "/" + mc.getMaxEnergy());
+		mcHealth.setProgress((float)mc.getHealth()/mc.getMaxHealth());
+		mcEnergy.setProgress((float)mc.getEnergy()/mc.getMaxEnergy());
 		
 		partnerName.setText(partner.getName());
 		partnerClass.setText(partner.getClass().getSimpleName().substring(2));
 		partnerAttack.setText("ATK: " + partner.getAttack());
-		partnerHealth.setText(partner.getHealth() + "/" + partner.getMaxHealth());
-		partnerEnergy.setText(partner.getEnergy() + "/" + partner.getMaxEnergy());
+		partnerHealth.setProgress((float)partner.getHealth()/partner.getMaxHealth());
+		partnerEnergy.setProgress((float)partner.getEnergy()/partner.getMaxEnergy());
 		
 		mcWeaponName.setText(mc.getWeapon().getName());
 		mcWeaponClass.setText("Class"); // We have't set-up class specific weapons yet
 		mcWeaponAttack.setText("ATK Bonus: " + mc.getWeapon().getAtkBonus());
-		mcWeaponDurability.setText(mc.getWeapon().getDurability() + "/" + mc.getWeapon().getMaxDurability());
+		mcWeaponDurability.setProgress((float)mc.getWeapon().getDurability() /mc.getWeapon().getMaxDurability());
 		
 		partnerWeaponName.setText(partner.getWeapon().getName());
 		partnerWeaponClass.setText("Class"); // We have't set-up class specific weapons yet
 		partnerWeaponAttack.setText("ATK Bonus: " + partner.getWeapon().getAtkBonus());
-		partnerWeaponDurability.setText(partner.getWeapon().getDurability() + "/" + partner.getWeapon().getMaxDurability());
+		partnerWeaponDurability.setProgress((float)partner.getWeapon().getDurability()/partner.getWeapon().getMaxDurability());
 	}
 }
