a0062bfaf94fd58f9c48dc9811a0104d2458cb09
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-03-23 10:57:55 +0800

    Update MapController.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/MapController.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/MapController.java
@@ -17,34 +17,9 @@ import javafx.event.ActionEvent;
 // Exceptions
 import java.io.IOException;
 
-public class MapController implements Initializable{
+public class MapController extends SuperController implements Initializable{
 	private String lastFileName;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {}
-	
-	private FXMLLoader openNewWindow(String fileName, ActionEvent e) throws IOException{
-		// Getting the current Window
-		Stage currentStage = (Stage)((Node) e.getSource()).getScene().getWindow();
-
-		// Getting the next Screen
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(fileName));
-		Parent root = loader.load();
-		Scene newScene = new Scene(root);
-		
-		// Updating the screen
-		currentStage.hide();
-		currentStage.setScene(newScene);
-		currentStage.show();
-		
-		return loader;
-	}
-	
-	public void updateLastScreen (String fileName) {
-		lastFileName = fileName;
-	}
-	
-	@FXML public void openPreviousScreen(ActionEvent e) throws IOException {
-		openNewWindow(lastFileName, e);
-	}
 }
