eea87836918775321575c08fa2712cf99937a625
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-03-27 16:14:36 +0800

    Update PrebattleController.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/PrebattleController.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/PrebattleController.java
@@ -13,24 +13,23 @@ import javafx.fxml.*;
 import javafx.scene.text.Text;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
-import javafx.stage.*;
 
 // Exception
 import java.io.IOException;
+import the_lucky_gauntlet.Exceptions.NoEnergyException;
 
 public class PrebattleController extends SuperController implements Initializable {
 	
 	@FXML Text actionText, movesLeftText, mcName, partnerName;
-        @FXML Button mcWeaponSelectButton, partnerWeaponSelectButton;
+	@FXML Button mcChangeWeaon, mcFindWeapon, mcEnhanceWeapon, mcRepairWeapon;
+	@FXML Button partnerChangeWeapon, partnerFindWeapon, partnerEnhanceWeapon, partnerRepairWeapon;
+	@FXML Button rest, train;
 	@FXML ImageView mcImage, partnerImage;
 	
 	private int actions;
 	
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		mc = Player.getMC();
-		partner = mc.getPartner();
-		
+	public void initialize(URL url, ResourceBundle rb) {		
 		actions = 3;
 		
 		actionText.setText(" ");
@@ -39,13 +38,80 @@ public class PrebattleController extends SuperController implements Initializabl
 		partnerName.setText(partner.getName());
 	}	
 	
-	@FXML void openPause(ActionEvent e) throws IOException{
-		FXMLLoader loader = openNewWindow("Pause.fxml", e);
-		PauseController PController = loader.getController();
+	private void performAction(String textOutput) {
+		actions -= 1;
+		movesLeftText.setText("Moves Left: " + actions);
+		actionText.setText(textOutput);
+		
+		if(actions == 0) {
+			mcFindWeapon.setDisable(true);
+			mcEnhanceWeapon.setDisable(true);
+			mcRepairWeapon.setDisable(true);
+			
+			partnerFindWeapon.setDisable(true);
+			partnerEnhanceWeapon.setDisable(true);
+			partnerRepairWeapon.setDisable(true);
+			
+			rest.setDisable(true);
+			train.setDisable(true);
+		}
 	}
-        
-        @FXML void openWeaponSelect(ActionEvent e) throws IOException{
-                FXMLLoader loader = openNewWindow("WeaponSelect.fxml", e);
-		WeaponSelectController WSController = loader.getController();
-        }
+	
+	@FXML void openPause(ActionEvent e) throws IOException{
+		openNewWindow("Pause.fxml", e);
+	}
+	
+	
+	@FXML void rest(ActionEvent e) throws IOException{
+		int mcInitialEnergy, mcFinalEnergy, partnerInitialEnergy, partnerFinalEnergy;
+		mcInitialEnergy = mc.getEnergy();
+		partnerInitialEnergy = partner.getEnergy();
+		
+		mc.rest();
+		
+		mcFinalEnergy = mc.getEnergy();
+		partnerFinalEnergy = partner.getEnergy();
+		String textOutput = mc.getName() + " and " + partner.getName() + " rested!\n"
+				+ "Their energies increased by " + (mcFinalEnergy-mcInitialEnergy) + " and " + (partnerFinalEnergy-partnerInitialEnergy)
+				+ " and are now " + mcFinalEnergy + " and " + partnerFinalEnergy + "!";
+
+		performAction(textOutput);
+	}
+	@FXML void train(ActionEvent e) throws IOException{
+		int mcInitialAtk, mcFinalAtk, partnerInitialAtk, partnerFinalAtk;
+		int mcInitialEnergy, mcFinalEnergy, partnerInitialEnergy, partnerFinalEnergy;
+		String textOutput;
+		
+		mcInitialAtk = mc.getAttack();
+		partnerInitialAtk = partner.getAttack();
+		mcInitialEnergy = mc.getEnergy();
+		partnerInitialEnergy = partner.getEnergy();
+		
+		try {
+			mc.train();
+			partner.train();
+			
+			mcFinalAtk = mc.getAttack();
+			partnerFinalAtk = partner.getAttack();
+			mcFinalEnergy = mc.getEnergy();
+			partnerFinalEnergy = partner.getEnergy();
+		
+			textOutput = mc.getName() + " and " + partner.getName() + " trained together!\n"
+				+ "Their atk power increased by " + (mcFinalAtk-mcInitialAtk) + " and " + (partnerFinalAtk-partnerInitialAtk)
+				+ " at the cost of 25 energy!\nThey now have an attack power of " + mcFinalAtk + " and " + partnerFinalAtk + "!";
+		}
+		catch(NoEnergyException NEE) {
+			mc.rest();
+			
+			mcFinalEnergy = mc.getEnergy();
+			partnerFinalEnergy = partner.getEnergy();
+			
+			textOutput = "Your party was too tired to train. They decided to rest and increased their energies by "
+					+ (mcFinalEnergy-mcInitialEnergy) + " and " + (partnerFinalEnergy-partnerInitialEnergy)
+					+ ".\nThey now have " + mcFinalEnergy + " and " + partnerFinalEnergy + " energy.";
+		}
+		
+		performAction(textOutput);		
+	}
+	
 }
