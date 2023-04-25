7585a53a0407b3abf67753456d3e0195e686fcf0
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-04-25 23:57:55 +0800

    Update R_Battle.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Rooms/R_Battle.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Rooms/R_Battle.java
@@ -15,84 +15,17 @@ public class R_Battle extends Room{
 	// Getter Methods
 	public ArrayList<Enemy> getAllEnemies(){
 		return this.enemies;
-  }
+	}
 	
 	// Enemy Set-up Methods
 	public void addEnemy(Enemy c){
 		enemies.add(c);
 		c.updateRoom(this);
-  }
+	}
 	public void fillRoom(int enemyCount){
 		for(int i=0; i<enemyCount; i++) {
 			Enemy enemy = new Enemy("Enemy " + (i+1));
 			this.addEnemy(enemy);
-		}
-  }
-	
-	// Room Actions Methods
-	public void performAction(Player mc) {
-		if (!completed){
-			System.out.println("Actions: ");
-			System.out.printf(" 1 - Attack\n"
-					+ " 2 - %s Attack\n"
-					+ " 3 - Use Skill\n"
-					+ " 4 - Use %s's Skill\n"
-					+ " 5 - Stall\n"
-					+ " 6 - Open Menu\n", mc.getPartnerName(),mc.getPartnerName());
-			System.out.print("What will you do? ");
-			int action = Integer.parseInt(sc.nextLine());
-
-			switch(action){
-				case 1:
-					mc.targetSelect(enemies);
-					mc.attack();
-					if(mc.getTarget().isDead()) {
-						enemies.remove(mc.getTarget());
-					}
-					break;
-				case 2:
-					mc.getPartner().targetSelect(enemies);
-					mc.getPartner().attack();
-					if(mc.getPartner().getTarget().isDead()) {
-						enemies.remove(mc.getTarget());
-					}
-					break;
-				case 3:
-					mc.useSkill();
-					break;
-				case 4:
-					mc.getPartner().useSkill();
-					break;
-				case 5:
-					mc.stall();
-					break;
-				case 6:
-					this.openMenu();
-					break;
-				default: 
-					System.out.println("There are no actions with that ID");
-					break;
-					//Action doesn't exist exception
-			}
-			completed = true;
-			
-			for (Enemy e : enemies) {
-				if(!(e.isDead())) {
-					completed = false;
-				}
-				e.targetSelect(players);
-				e.attack();
-			}
-		}
-		else {
-			System.out.println("Actions: ");
-			System.out.printf(" 1 - Leave Room\n");
-			System.out.print("What will you do? ");
-			int action = Integer.parseInt(sc.nextLine());
-
-			if(action == 1) {
-				mc.changeRoom(Room.getRoom("Main Hall"));
-			}
 		}
 	}
 }
