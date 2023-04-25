41ad9b57118e7a060f15c4122cb93d1dd8556ba7
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-04-26 00:00:31 +0800

    Update R_Peaceful.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Rooms/R_Peaceful.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Rooms/R_Peaceful.java
@@ -6,69 +6,46 @@ import the_lucky_gauntlet.*;
 import the_lucky_gauntlet.Exceptions.NoEnergyException;
 
 public class R_Peaceful extends Room{
-	private int actionCount;
+	private int actions;
 	
 	public R_Peaceful(String n, boolean u, int aC) {
 		super(n, u);
 		actionCount = aC;
 	}
 	
-	public void performAction(Player mc) {
-		System.out.println("Actions: ");
-		System.out.printf(" 1 - Leave Room\n");
-		if(actionCount > 0) {
-		System.out.printf(" 2 - Train\n"
-						+ " 3 - Train %s\n"
-						+ " 4 - Rest\n"
-						+ " 5 - Open Menu\n", mc.getPartnerName());
+	// Getter Methods
+	public int getActions() {
+		return actions;
+	}
+	
+	// Group Actions
+	public void train() throws NoEnergyException {		
+		try {
+			tlg.mc.train();
+			tlg.partner.train();
+			actions--;
 		}
-		System.out.print("What will you do? ");
-		int action = Integer.parseInt(sc.nextLine());
-
-		
-		if(action == 1) {
-			System.out.println(Room.getRoom("Main Hall").getName());
-			mc.changeRoom(Room.getRoom("Main Hall"));
-		}
-		else if(actionCount > 0) {
-			switch(action){
-				case 2:
-                            {
-                                try {
-                                    mc.train();
-                                } catch (NoEnergyException ex) {
-                                    System.out.println("No more energy");
-                                }
-                            }
-					actionCount -= 1;
-					break;
-
-				case 3:
-                            {
-                                try {
-                                    mc.getPartner().train();
-                                } catch (NoEnergyException ex) {
-                                    System.out.println("No more energy");
-                                }
-                            }
-					actionCount -= 1;
-					break;
-
-				case 4:
-					mc.rest();
-					actionCount -= 1;
-					break;
-				case 5:
-					this.openMenu();
-					break;
-				default: 
-					System.out.println("There are no actions with that ID");
-					break;
-					//Action doesn't exist exception
-			}
-		}
-		else {
-			System.out.println("Enough hiding. It is time to re-enter the dungeon.");
-		}
+		catch (NoEnergyException NEE) {
+			tlg.mc.rest();
+			throw new NoEnergyException();
+		}		
+	}
+	public void rest() {
+		tlg.mc.rest();
+		actions--;
+	}
+	
+	// Individual Actions
+	public void repairWeapon(Player p) {
+		p.getWeapon().repair();
+		actions -= 1;
+	}
+	public void enhanceWeapon(Player p) {
+		p.getWeapon().enhance();
+		actions -= 1;
+	}
+	public void findWeapon(Player p) {
+		p.newWeapon();
+		actions -= 1;
 	}
 }
