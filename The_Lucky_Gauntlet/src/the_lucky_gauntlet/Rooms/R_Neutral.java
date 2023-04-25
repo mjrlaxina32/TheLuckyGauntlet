c299451abef72ffc14734f01a8c2f5261e72a051
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-04-25 23:58:27 +0800

    Update R_Neutral.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Rooms/R_Neutral.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Rooms/R_Neutral.java
@@ -17,23 +17,4 @@ public class R_Neutral extends Room {
 	public void attachRooms(ArrayList<Room> rooms) {
 		adjacentRooms.addAll(rooms);
 	}
-	
-	// Room actions
-	public void performAction(Player mc) {
-		System.out.println("Actions: ");
-		int lastIndex = 0;
-		for(int i=0;i<adjacentRooms.size();i++){
-			System.out.printf(" %d - Enter %s\n", i+1, adjacentRooms.get(i).getName());
-			lastIndex = i+2;
-		}
-		System.out.printf(" %d - Open Menu\n", lastIndex);
-		int action = Integer.parseInt(sc.nextLine())-1;
-
-		if((action+1) == lastIndex) {
-			this.openMenu();
-		}
-		else {
-			mc.changeRoom(adjacentRooms.get(action));
-		}
-	}
 }
