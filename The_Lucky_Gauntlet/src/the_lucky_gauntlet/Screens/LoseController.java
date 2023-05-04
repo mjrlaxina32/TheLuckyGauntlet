395e1f64c69440974093ac099d85ddab277dbcc7
athena-k <97677736+athena-k@users.noreply.github.com>
2023-05-04 11:29:26 +0800

    Add files via upload

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/LoseController.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/LoseController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -21,6 +22,7 @@ import javafx.scene.layout.VBox;
  */
 public class LoseController extends SuperController implements Initializable {
     @FXML VBox BoxLayout;
+    @FXML Text roundNo, loseMsg, gameOverMsg;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {}
@@ -31,10 +33,16 @@ public class LoseController extends SuperController implements Initializable {
 
     @FXML private void setBlackBG(Event event)throws IOException{
         BoxLayout.setStyle("-fx-background-color: black");
+        roundNo.setStyle("-fx-fill: white");
+        loseMsg.setStyle("-fx-fill: white");
+        gameOverMsg.setStyle("-fx-fill: white");
     }
     
     @FXML private void setWhiteBG(Event event)throws IOException{
         BoxLayout.setStyle("-fx-background-color: white");
+        roundNo.setStyle("-fx-fill: black");
+        loseMsg.setStyle("-fx-fill: black");
+        gameOverMsg.setStyle("-fx-fill: black");
         //sets bg to white and not the prev/default bg color
     }
 }
