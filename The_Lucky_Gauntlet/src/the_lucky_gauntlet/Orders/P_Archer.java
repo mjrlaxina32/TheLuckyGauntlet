310703b2a7f098757cdbb22a41734df2f80f3203
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-05-05 11:03:54 +0800

    Update P_Archer.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Orders/P_Archer.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Orders/P_Archer.java
@@ -30,12 +30,23 @@ public class P_Archer extends Player {
 		try {
 			this.useEnergy(cost);
 			if (effects.contains("Hawkeye")) {
-				if (rand.nextInt(2) == 1) {
+				if (rand.nextBoolean()) {
 					System.out.printf("%s fired a perfect shot and landed a critical " +
 							"hit dealing %d damage!", this.getName(),
 							(attack + weapon.getAtkBonus()) * 2);
 					this.useEnergy(cost);
 					this.getTarget().takeDamage((attack + weapon.getAtkBonus()) * 2);
+					
+					if (target.isDead()){
+						for(Enemy e : tlg.currentRoom.getAllEnemies()) {
+							if(!e.isDead()) {
+								target = e;
+								tlg.partner.targetSelect(target);
+								System.out.println("Target has changed");
+								break;
+							}
+						}
+					}
 				}
 				else {
 					super.attack();
