686c968db1b3254e7a7c1733da98af5032c4a778
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2023-05-21 01:01:56 +0800

    Update Weapon.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Weapon.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Weapon.java
@@ -1,21 +1,38 @@
 package the_lucky_gauntlet;
 
 import java.util.Random;
+import javafx.scene.image.Image;
 import the_lucky_gauntlet.Exceptions.*;
 
 public class Weapon {
-	private String name, order;
+	private String name, order, filename;
 	protected int atkBonus, durability, maxDurability;
 	private Random rand = new Random();
 
-	public Weapon(String n, String o, int aB, int mD) {
+        public Weapon(String n, int aB, int mD) {
 		name = n;
+		atkBonus = aB;
+		maxDurability = mD;
+		durability = maxDurability;
+	} //!
+         
+	public Weapon(String n, String f, String o, int aB, int mD) {
+		name = n;
+                filename = f;
 		order = o;
 		atkBonus = aB;
 		maxDurability = mD;
 		durability = maxDurability;
 	}
 	
+        public Weapon(String n, String o, int aB, int mD) {
+		name = n;
+		order = o;
+		atkBonus = aB;
+		maxDurability = mD;
+		durability = maxDurability;
+	}
+        
 	public String getName (){
 		return name;
 	}
@@ -32,7 +49,16 @@ public class Weapon {
 		return order;
 	}
 	
+        public String getImgFileName() {
+                return filename;
+        } //to do: initialize file name
 	
+        public Image getImg() {
+		Image classImg = new Image("/the_lucky_gauntlet/Sprites/" + this.getImgFileName());
+		return classImg;
+	}
+        
+            
 	public void enhance() {
 		atkBonus += rand.nextInt(5);
 	}
