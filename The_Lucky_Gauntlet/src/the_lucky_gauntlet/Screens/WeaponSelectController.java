72ad2627934eec7b205bb991bd898fbd84a14d89
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2023-05-21 01:06:00 +0800

    Update WeaponSelectController.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/WeaponSelectController.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Screens/WeaponSelectController.java
@@ -5,14 +5,18 @@
  */
 package the_lucky_gauntlet.Screens;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import java.util.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.*;
 import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
+import javafx.scene.image.*;
 import javafx.scene.text.Text;
-
+import javafx.stage.Stage;
+import static the_lucky_gauntlet.Screens.SuperController.actionsource;
+import the_lucky_gauntlet.*;
 /**
  * FXML Controller class
  *
@@ -20,14 +24,114 @@ import javafx.scene.text.Text;
  */
 public class WeaponSelectController extends SuperController implements Initializable {
 
-    @FXML Text weaponSelectATK, weaponSelectDurabilityText;
+    @FXML Text weaponSelectName, weaponSelectATK, weaponSelectDurabilityText;
     @FXML ProgressBar weaponSelectDurabilityBar;
     @FXML Button prevWeaponButton, nextWeaponButton, equipWeaponButton, returnButton;
     @FXML ImageView weaponSelectImage;
-    
+    private Label label;
+    private int curr;
+    private Weapon disWeapon, eqWeapon;
+    private Player currUser;
+    private String playerBr = actionsource;
+    private ArrayList weaponList;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
+            switch (playerBr) {
+            case "mc":
+                currUser = tlg.mc;
+                break;
+            case "partner":
+                currUser = tlg.partner;
+                break;
+            default:
+                currUser = tlg.partner;
+                break;
+            } 
+        update(0); 
     }    
+        
+     public void update(int curr){
+        this.curr = curr;
+        eqWeapon = currUser.getWeapon();
+        System.out.println("eqW: "+ eqWeapon.getName());
+        disWeapon = currUser.getWeaponFromList(curr); 
+        weaponSelectName.setText(disWeapon.getName());
+        weaponSelectATK.setText("ATK Bonus: " + String.valueOf(disWeapon.getAtkBonus()));
+        weaponSelectDurabilityBar.setProgress((float)disWeapon.getDurability() /disWeapon.getMaxDurability());      
+        weaponSelectDurabilityText.setText(String.valueOf(disWeapon.getDurability()) + "/" + String.valueOf(disWeapon.getMaxDurability()));
+        weaponSelectImage.setImage(disWeapon.getImg());
+        weaponList = currUser.getWeaponList();
+        
+        for(int i = 0; i < weaponList.size(); i++) {
+            Weapon l = (Weapon) weaponList.get(i);
+            System.out.println(l.getName());
+        }  
+        
+        int ind = currUser.getWeaponList().size()-1;
+        if(curr<ind){
+            nextWeaponButton.setDisable(false); 
+        }
+        else{
+            nextWeaponButton.setDisable(true);
+        }
+        
+        if(curr>0){
+            prevWeaponButton.setDisable(false); 
+        }
+        else{
+            prevWeaponButton.setDisable(true);
+        }
+        System.out.println("pg."+ curr + " |updated");
+        
+        if(disWeapon == eqWeapon){
+            equipWeaponButton.setDisable(true); 
+        }
+        else{
+            equipWeaponButton.setDisable(false);
+        }
+    }
     
-}
+    @FXML private void next(){
+        curr++;
+        update(curr);
+        System.out.println("pg."+ curr + " |updating");
+    }
+    
+    @FXML private void prev(){
+        curr--;
+        update(curr);
+        System.out.println("pg."+ curr + " |updating");
+    }
+    
+    @FXML private void equip(){
+        eqWeapon = disWeapon;
+        currUser.changeWeapon(curr);
+        update(curr);
+    }
+    
+    @Override 
+    @FXML public void openPreviousWindow(ActionEvent e) throws IOException {
+		switch (playerBr) {
+                case "mc":
+                    tlg.mc = currUser;
+                    break;
+                case "partner":
+                    tlg.partner = currUser;
+                    break;
+                default:
+                    tlg.mc = currUser;
+                    break;
+                }
+                actionsource = "x";
+                int hierarchyLength = stageHierarchy.size();
+		Stage currentStage = stageHierarchy.get(hierarchyLength-1);
+		Stage previousStage = stageHierarchy.get(hierarchyLength-2);
+		
+		currentStage.hide();
+		previousStage.show();
+		
+		stageHierarchy.remove(currentStage);
+	}
+}   
+    
