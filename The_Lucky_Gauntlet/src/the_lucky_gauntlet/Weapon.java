da6ceeb7ab56bef2cb0b8f5249874a39a51f888d
mjrlaxina32 <96271947+mjrlaxina32@users.noreply.github.com>
2023-05-23 08:08:59 +0800

    Update Weapon.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/Weapon.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/Weapon.java
@@ -4,19 +4,37 @@ import java.util.Random;
 import javafx.scene.image.Image;
 import the_lucky_gauntlet.Exceptions.*;
 
+/**
+ *A weapon has a name, attack bonus, and max durability. It can also have a file name and belong to an order. 
+ * The weapon's main purpose is to increase the attack value of the player.
+ * @author Monique Jhoienyl
+ * @version 5-23-23
+ */
 public class Weapon {
-	private String name, order, filename;
-	protected int atkBonus, durability, maxDurability;
-	private Random rand = new Random();
-
-        public Weapon(String n, int aB, int mD) {
+    private String name, order, filename;
+    protected int atkBonus,durability, maxDurability;
+    private Random rand = new Random();
+    
+    /**
+     * @param n Name of weapon
+     * @param aB Value of weapon Attack Bonus
+     * @param mD Max Durability of weapon
+     */
+    public Weapon(String n, int aB, int mD) {
 		name = n;
 		atkBonus = aB;
 		maxDurability = mD;
 		durability = maxDurability;
 	} //!
          
-	public Weapon(String n, String f, String o, int aB, int mD) {
+    /**
+     * @param n Name of weapon
+     * @param f Filename of weapon
+     * @param o Order of weapon
+     * @param aB Value of weapon Attack Bonus
+     * @param mD Max Durability of weapon
+     */
+    public Weapon(String n, String f, String o, int aB, int mD) {
 		name = n;
                 filename = f;
 		order = o;
@@ -25,7 +43,13 @@ public class Weapon {
 		durability = maxDurability;
 	}
 	
-        public Weapon(String n, String o, int aB, int mD) {
+    /**
+     * @param n Name of weapon
+     * @param o Order of weapon
+     * @param aB Value of weapon Attack Bonus
+     * @param mD Max Durability of weapon
+     */
+    public Weapon(String n, String o, int aB, int mD) {
 		name = n;
 		order = o;
 		atkBonus = aB;
@@ -33,36 +57,68 @@ public class Weapon {
 		durability = maxDurability;
 	}
         
-	public String getName (){
+    /** 
+     * @return Returns weapon name
+     */
+    public String getName (){
 		return name;
 	}
-	public int getAtkBonus (){
+
+    /** 
+     * @return Returns weapon attack bonus
+     */
+    public int getAtkBonus (){
 		return atkBonus;
 	}
-	public int getDurability (){
+
+    /** 
+     * @return Returns weapon current durability
+     */
+    public int getDurability (){
 		return durability;
 	}
-	public int getMaxDurability (){
+
+    /** 
+     * @return Returns weapon max durability
+     */
+    public int getMaxDurability (){
 		return maxDurability;
 	}
-	public String getOrder (){
+
+    /** 
+     * @return Returns order of weapon
+     */
+    public String getOrder (){
 		return order;
 	}
 	
-        public String getImgFileName() {
+    /** 
+     * @return Returns weapon filename
+     */
+    public String getImgFileName() {
                 return filename;
         } //to do: initialize file name
 	
-        public Image getImg() {
+    /**
+     * @return Returns weapon image using weapon filename 
+     */
+    public Image getImg() {
 		Image classImg = new Image("/the_lucky_gauntlet/Sprites/" + this.getImgFileName());
 		return classImg;
 	}
         
-            
-	public void enhance() {
+    /**
+     * Adds a random number between 0-5 to the attack bonus of the weapon
+     */
+    public void enhance() {
 		atkBonus += rand.nextInt(5);
 	}
-	public void repair() throws BeyondRangeException{
+
+    /**
+     * Increases current durability of the weapon by 10
+     * @throws BeyondRangeException if the durability is equal to the max durability 
+     */
+    public void repair() throws BeyondRangeException{
 		if(durability == maxDurability){
 			throw new BeyondRangeException("The weapon has reached its maximum durability.");
 		}
@@ -70,14 +126,13 @@ public class Weapon {
 		durability += 10;
 		}
 	}
-	public void weaken() {
+
+    /**
+     *Decreases current durability of the weapon by 5
+     */
+    public void weaken() {
 		durability -= 5;
 		if(durability <= 0)
 			durability = 0;
-	}
-	public void destroy() {
-		//How do we destroy a weapon?
-		//not too sure but
-		//durability = 0;
 	}
 }
