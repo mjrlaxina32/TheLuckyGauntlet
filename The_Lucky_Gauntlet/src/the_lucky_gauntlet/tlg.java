950ec92b28a5fbcb5ab58fb28d6a3d835b25fcc6
Adam-Lanuza <b2025ajdlanuza@pshs.edu.ph>
2023-05-26 01:29:57 +0800

    Update tlg.java

--- The_Lucky_Gauntlet/src/the_lucky_gauntlet/tlg.java
+++ The_Lucky_Gauntlet/src/the_lucky_gauntlet/tlg.java
@@ -1,12 +1,21 @@
 package the_lucky_gauntlet;
 
+// Utility
 import java.util.Random;
+
+// JavaFX Components
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+// Events
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
+
+// Lucky Gaunltet Imports
 import the_lucky_gauntlet.Exceptions.NoEnergyException;
 import the_lucky_gauntlet.Exceptions.InvalidOrderException;
 import the_lucky_gauntlet.Orders.*;
@@ -38,6 +47,13 @@ public class tlg extends Application {
 	
 	@Override
 	public void start(Stage stage) throws Exception{
+		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent e) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
 		Parent root = FXMLLoader.load(getClass().getResource("Screens/Home.fxml"));
 		Scene scene = new Scene(root);
 		
@@ -50,15 +66,7 @@ public class tlg extends Application {
 	}
 
 	public static void main(String[] args) {
-		for(int i = 0; i<19; i++) {
-			Random rand = new Random();
-			if(rand.nextBoolean()) {
-				new R_Peaceful("Room " + i, true, 3);
-			}
-			else {
-				new R_Battle("Room " + i, true);
-			}
-		}
+		Room.resetRooms();
 		mc = new P_Archer("Henry", "mc");
 		partner = new P_Rogue("Joe", "partner");
 		
